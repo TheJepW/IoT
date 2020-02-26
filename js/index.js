aa5e91b252a4bf37bdc717d04b5519f1a14a3f03
TheJepW <34625606+TheJepW@users.noreply.github.com>
2020-02-26 09:03:11 +0100

    from the top

--- js/index.js
+++ js/index.js
@@ -57,11 +57,9 @@ function refreshDeviceList(){
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
 		var listItem = document.createElement('li'),
-		if(device.name == "IoT_MJM"){
-			html = device.name+ "," + device.id;
-			listItem.innerHTML = html;
-			document.getElementById("bleDeviceList").appendChild(listItem);
-		}
+		html = device.name+ "," + device.id;
+		listItem.innerHTML = html;
+		document.getElementById("bleDeviceList").appendChild(listItem);
 }
 
 
